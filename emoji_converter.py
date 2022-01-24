c6169e1e3402cb12bc0a1570cb17ffa6ec826dc1
Rubait Reshad <43026478+thinkGrow@users.noreply.github.com>
2022-01-25 04:32:34 +0600

    Add files via upload
    Added emoji_convert.py file

---
+++ emoji_converter.py
@@ -0,0 +1,15 @@
+msg = input(">")
+words = msg.split(' ')
+
+emojis = {
+    ":)": "🙂",
+    ":(": "😞"
+}
+
+output = ""
+
+for word in words:
+
+    output += emojis.get(word, word) + " "
+
+print(output)
